5a8537700c7f2db013f824e9b2b5cc5f495b5570
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-16 14:53:20 +0500

    converting string to lowercase and uppercase

---
+++ examples/stringConvertingLowerandUpperCase.rb
@@ -0,0 +1,34 @@
+# input the string
+puts "Enter the string"
+str=gets.chomp
+
+#ask for the option
+puts "Choose the option: a)to lowercase b)to uppercase"
+choice=gets.chomp
+
+# condition to execute code based on the user's choice
+if(choice=="a")
+	i=0
+	while(i!=str.size)
+	    k=str[i].to_s
+	    if(k>='A' and k<='Z')
+	        str[i]=(k.ord+32).chr
+        else
+            str[i]=k
+        end
+	    i+=1
+	end
+	puts "The lowercase is #{str}"
+else
+	i=0
+	while(i!=str.size)
+	    k=str[i].to_s
+	    if(k>='a' and k<='z')
+	        str[i]=(k.ord-32).chr
+	    else
+	        str[i]=k;
+	    end
+	    i+=1
+	end
+	puts "The uppercase is #{str}"
+end
